608a70c4a6f29bb3e46ccd148abcf168e4836724
Kössi <evkossi@gmail.com>
2016-04-24 21:15:58 +0300

    tenniksen refaktorointi

--- Tennis/src/main/java/ohtu/TennisGame.java
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -4,62 +4,50 @@ public class TennisGame {
     
     private int m_score1 = 0;
     private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private final String player1Name;
+    private final String player2Name;
+    private final String[] gameStatus = new String[5];
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
+        setPointTable();
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals("player1"))
             m_score1 += 1;
         else
             m_score2 += 1;
     }
+    
+    public void setPointTable(){
+        gameStatus[0] = "Love";
+        gameStatus[1] = "Fifteen";
+        gameStatus[2] = "Thirty";
+        gameStatus[3] = "Forty";
+        gameStatus[4] = "Deuce";
+    }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            score = tasaspelitilanne(score);
+        if (m_score1==m_score2) {
+            return evenGameScore(score);
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            score = pistetilanneYliNeljan(score);
+        if (m_score1>=4 || m_score2>=4) {
+            return winGamePointScore(score);
         }
-        else
-        {   
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                
-                score = tilannepalaute(tempScore, score);
-            }
-        }
-        return score;
+        return currentGameScore(score);
     }
     
-    public String tasaspelitilanne(String score) {
-        switch (m_score1)
-            {
-                case 0:
-                        return "Love-All";
-                case 1:
-                        return "Fifteen-All";
-                case 2:
-                        return "Thirty-All";
-                case 3:
-                        return "Forty-All";
-                default:
-                        return "Deuce";          
-            }
+    public String evenGameScore(String score) {
+        if (m_score1 < 4) {
+            return gameStatus[m_score1]+"-All";
+        }
+        return gameStatus[4];
     }
     
-    public String pistetilanneYliNeljan(String score) {
+    public String winGamePointScore(String score) {
         int minusResult = m_score1-m_score2;
         if (minusResult==1) return "Advantage player1";
         if (minusResult ==-1) return "Advantage player2";
@@ -67,41 +55,13 @@ public class TennisGame {
         return score ="Win for player2";
     }
     
-    public String muuTilanne(String score){
-        int tempScore=0;
-        for (int i=1; i<3; i++)
-            {
+    public String currentGameScore(String score) {
+        int tempScore = 0;
+        for (int i=1; i<3; i++) {
                 if (i==1) tempScore = m_score1;
                 else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score += "Love";
-                        //return score+"Love";
-                        return score;
-                    case 1:
-                        return score+"Fifteen";
-                    case 2:
-                        return score+"Thirty";
-                    case 3:
-                        return score+"Forty";
-                }
+                score += gameStatus[tempScore];
             }
-        return score;
-    }
-    
-    public String tilannepalaute(int tempScore, String score) {
-        switch(tempScore)
-                {
-                    case 0:
-                        return score+"Love";
-                    case 1:
-                        return score+"Fifteen";
-                    case 2:
-                        return score+"Thirty";
-                    case 3:
-                        return score+"Forty";
-        }
         return score;
     }
 }
